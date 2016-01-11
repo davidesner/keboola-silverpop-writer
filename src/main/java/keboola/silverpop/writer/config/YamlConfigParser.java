95a3dc6c3fa6c9fc85f6b6ed1b5828a838640ef8
davidesner <esnerda@gmail.com>
2016-01-11 11:09:08 +0100

    Fix: Ignore unknown config properties

--- src/main/java/keboola/silverpop/writer/config/YamlConfigParser.java
+++ src/main/java/keboola/silverpop/writer/config/YamlConfigParser.java
@@ -2,6 +2,7 @@
  */
 package keboola.silverpop.writer.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
@@ -16,6 +17,7 @@ public class YamlConfigParser {
 
     public static KBCConfig parseFile(File file) throws IOException {
         final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper.readValue(file, KBCConfig.class);
     }
 }
